9ce5783fb2fedb8a9661cc6a0aad4126b7465407
SalmaAtefSaeid <salma.atef.ebrahim@gmail.com>
2019-04-16 15:49:22 +0200

    add trip bean

---
+++ app/src/main/java/com/fekratoday/mashawer/model/database/TripDao.java
@@ -0,0 +1,12 @@
+package com.fekratoday.mashawer.model.database;
+
+import com.fekratoday.mashawer.model.beans.Trip;
+
+public class TripDao {
+
+
+    public boolean insertTrip(Trip trip){
+        boolean inserted = false;
+        return  inserted;
+    }
+}
