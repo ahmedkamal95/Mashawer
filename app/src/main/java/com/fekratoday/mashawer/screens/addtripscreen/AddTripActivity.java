c5f2fd89c63e93e9d0a12f1029873e0649632fad
rokayaismail <roka.moon134@gmail.com>
2019-04-18 22:55:41 +0200

    Add view initial version for testing purposes finished

--- app/src/main/java/com/fekratoday/mashawer/screens/addtripscreen/AddTripActivity.java
+++ app/src/main/java/com/fekratoday/mashawer/screens/addtripscreen/AddTripActivity.java
@@ -1,20 +1,39 @@
 package com.fekratoday.mashawer.screens.addtripscreen;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.fekratoday.mashawer.R;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
+import java.util.Calendar;
+
 public class AddTripActivity extends AppCompatActivity {
 
     PlaceAutocompleteFragment startPlaceAutocompleteFragment, endPlaceAutocompleteFragment;
-
+    private TextView tripTime,tripDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +46,8 @@ public class AddTripActivity extends AppCompatActivity {
     private void initPlaceSearch(){
         startPlaceAutocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment_from);
         endPlaceAutocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment_to);
-        AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder().setTypeFilter(5).build();
-
-        startPlaceAutocompleteFragment.setFilter(autocompleteFilter);
+//        AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder().setTypeFilter(5).build();
+//        startPlaceAutocompleteFragment.setFilter(autocompleteFilter);
         startPlaceAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
@@ -43,12 +61,13 @@ public class AddTripActivity extends AppCompatActivity {
             }
         });
 
-        endPlaceAutocompleteFragment.setFilter(autocompleteFilter);
+//        endPlaceAutocompleteFragment.setFilter(autocompleteFilter);
         endPlaceAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 Toast.makeText(AddTripActivity.this, String.valueOf(place.getLatLng().longitude), Toast.LENGTH_SHORT).show();
                 Toast.makeText(AddTripActivity.this, String.valueOf(place.getLatLng().latitude), Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -56,5 +75,68 @@ public class AddTripActivity extends AppCompatActivity {
                 Toast.makeText(AddTripActivity.this, "error", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void addNote(View v){
+        EditText noteView = findViewById(R.id.noteField);
+        TextView noteAddedNote = new TextView(AddTripActivity.this);
+        LinearLayout noteList=findViewById(R.id.noteList);
+        noteAddedNote.setText(noteView.getText());
+        noteList.addView(noteAddedNote);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            TextView date = getActivity().findViewById(R.id.tripDateField);
+            date.setText(day +"/"+(month+1)+"/"+year);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            TextView time = getActivity().findViewById(R.id.tripTimeField);
+            time.setText(hourOfDay +":"+minute);
+        }
     }
 }
