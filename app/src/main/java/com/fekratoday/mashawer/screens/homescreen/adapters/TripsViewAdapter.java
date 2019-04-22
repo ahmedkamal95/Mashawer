2bbfb345957fa71f6a77be719815f753ff98fd1a
rokayaismail <roka.moon134@gmail.com>
2019-04-22 02:03:42 +0200

    customized card view

--- app/src/main/java/com/fekratoday/mashawer/screens/homescreen/adapters/TripsViewAdapter.java
+++ app/src/main/java/com/fekratoday/mashawer/screens/homescreen/adapters/TripsViewAdapter.java
@@ -1,40 +1,71 @@
 package com.fekratoday.mashawer.screens.homescreen.adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.fekratoday.mashawer.R;
 import com.fekratoday.mashawer.model.beans.Trip;
+import com.fekratoday.mashawer.screens.homescreen.HomeActivity;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class TripsViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<Trip> tripList;
-
-    public TripsViewAdapter(List<Trip> tripList) {
+    private Activity currentActivity;
+    public TripsViewAdapter(FragmentActivity activity, List<Trip> tripList) {
         this.tripList = tripList;
+        currentActivity = activity;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        LayoutInflater inflater= (LayoutInflater) viewGroup.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view =inflater.inflate(R.layout.trip_row_layout,viewGroup,false);
+        LayoutInflater inflater = (LayoutInflater) viewGroup.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.trip_row_layout, viewGroup, false);
         return new TripViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-        TextView titleView = viewHolder.itemView.findViewById(R.id.titleView);
+        TextView titleView = viewHolder.itemView.findViewById(R.id.tripName);
         titleView.setText(tripList.get(i).getName());
-        TextView detailsView =viewHolder.itemView.findViewById(R.id.detailsView);
-        detailsView.setText(tripList.get(i).getStartPoint());
+        TextView detailsView = viewHolder.itemView.findViewById(R.id.tripLine);
+        detailsView.setText("From: " + tripList.get(i).getStartPoint() + ",  To: " + tripList.get(i).getEndPoint());
+        TextView timeView = viewHolder.itemView.findViewById(R.id.tripTime);
+        int day = tripList.get(i).getDay();
+        int mon = tripList.get(i).getMonth();
+        int year = tripList.get(i).getYear();
+        Calendar calender = Calendar.getInstance();
+        calender.set(year, mon, day);
+        timeView.setText(calender.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault()) +
+                ", " + day + " " + calender.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) +
+                " " + year + ", " + String.format("%02d", tripList.get(i).getHour()) + ":" +
+                String.format("%02d", tripList.get(i).getMinute()));
+        LinearLayout noteView = viewHolder.itemView.findViewById(R.id.notesView);
+        if(tripList.get(i).getNotesList().size() > 0) {
+            for (Trip.Note note : tripList.get(i).getNotesList()) {
+                CheckBox checkBox = new CheckBox(currentActivity);
+                checkBox.setText(note.getNoteBody());
+                noteView.addView(checkBox);
+            }
+        }
+        else{
+            TextView textView = new TextView(currentActivity);
+            textView.setText("Empty Notes");
+            noteView.addView(textView);
+        }
     }
 
     @Override
@@ -42,22 +73,21 @@ public class TripsViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         return tripList.size();
     }
 
-    private class TripViewHolder extends RecyclerView.ViewHolder{
+    private class TripViewHolder extends RecyclerView.ViewHolder {
         private View tripView;
+
         TripViewHolder(@NonNull final View itemView) {
             super(itemView);
             tripView = itemView;
-            final ImageButton myBtn =tripView.findViewById(R.id.expandBtn);
-            tripView.findViewById(R.id.expandBtn).setOnClickListener(v -> {
-                if((itemView.findViewById(R.id.detailsView).getVisibility())== View.GONE){
-                    itemView.findViewById(R.id.detailsView).setVisibility(View.VISIBLE);
-                    itemView.findViewById(R.id.divider).setVisibility(View.VISIBLE);
+            final ImageButton myBtn = tripView.findViewById(R.id.expandBtn);
+            myBtn.setOnClickListener(v -> {
+                if ((itemView.findViewById(R.id.group).getVisibility()) == View.GONE) {
+                    itemView.findViewById(R.id.group).setVisibility(View.VISIBLE);
                     myBtn.setImageResource(R.drawable.round_keyboard_arrow_up_24px);
+                } else {
+                    itemView.findViewById(R.id.group).setVisibility(View.GONE);
+                    myBtn.setImageResource(R.drawable.round_keyboard_arrow_down_24px);
                 }
-                else{
-                    itemView.findViewById(R.id.detailsView).setVisibility(View.GONE);
-                    itemView.findViewById(R.id.divider).setVisibility(View.GONE);
-                    myBtn.setImageResource(R.drawable.round_keyboard_arrow_down_24px);}
             });
         }
     }
--- app/src/main/java/com/fekratoday/mashawer/screens/homescreen/fragments/TripsHistoryFragment.java
+++ app/src/main/java/com/fekratoday/mashawer/screens/homescreen/fragments/TripsHistoryFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.fekratoday.mashawer.R;
 import com.fekratoday.mashawer.model.beans.Trip;
+import com.fekratoday.mashawer.model.database.TripDaoSQL;
 import com.fekratoday.mashawer.screens.homescreen.adapters.TripsViewAdapter;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,7 @@ public class TripsHistoryFragment extends Fragment {
     private RecyclerView tripsView;
     private List<Trip> tripList;
     private TripsHistoryContract tripsHistoryContract;
+    private TripDaoSQL tripDaoSQL;
 
     public TripsHistoryFragment() {
     }
@@ -33,17 +35,19 @@ public class TripsHistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View tripHistoryFragmentView = inflater.inflate(R.layout.fragment_trips_history, container, false);
         tripsView = tripHistoryFragmentView.findViewById(R.id.tripsView);
-        tripList = new ArrayList<>();
+        tripDaoSQL = new TripDaoSQL(getActivity());
+        tripList = tripDaoSQL.getAllTrips();
 //        tripList.add(new Trip("Aswan","aswan aswan aswan"));
 //        tripList.add(new Trip("Luxur","Luxur Luxur Luxur"));
 //        tripList.add(new Trip("ITI","ITI ITI ITI"));
 //        tripList.add(new Trip("Aurgada","Aurgada Aurgada Aurgada"));
 
-        tripsHistoryContract = new TripsHistoryPresenterImpl(getContext());
-        tripList = tripsHistoryContract.getallTripsFirebase();
+//        tripsHistoryContract = new TripsHistoryPresenterImpl(getContext());
+//        tripList = tripsHistoryContract.getallTripsFirebase();
+
         manager = new LinearLayoutManager(getContext());
         tripsView.setLayoutManager(manager);
-        tripsAdapter = new TripsViewAdapter(tripList);
+        tripsAdapter = new TripsViewAdapter(getActivity(),tripList);
         tripsView.setAdapter(tripsAdapter);
         return tripHistoryFragmentView;
     }
